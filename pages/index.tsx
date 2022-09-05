8bd7d9cf046850c7c46c537d9316cf10bc3215ec
dmosc <osdavrm@gmail.com>
2022-09-05 17:50:09 -0500

    Move Chats view into `index.tsx`.
    Now, the `<Home />` component inside `index.tsx` servers the repos at the root path.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,36 @@
-import type { NextPage } from "next";
-import Chats from "./chats";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import github from "@services/github";
+import { Endpoints } from "@octokit/types";
+import styles from "./index.module.css";
+import Link from "next/link";
 
-const Root: NextPage = () => {
-  return <Chats />;
+const Home: NextPage = () => {
+  const [repos, setRepos] = useState<
+    Endpoints["GET /user/repos"]["response"]["data"]
+  >([]);
+  useEffect(() => {
+    github.query("/user/repos?sort=updated").then(({ data }) => setRepos(data));
+  }, []);
+  return (
+    <div className={styles.grid}>
+      <h1 className={styles.gridTitle}>Recent changes</h1>
+      {repos.map((repo) => (
+        <Link
+          key={repo.id}
+          href={{
+            pathname: "/explorer",
+            query: { contentsUrl: repo.contents_url }
+          }}
+        >
+          <div className={styles.card}>
+            <div className={styles.cardTitle}>{repo.name}</div>
+            <div>{repo.description ?? "..."}</div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
-export default Root;
+export default Home;
