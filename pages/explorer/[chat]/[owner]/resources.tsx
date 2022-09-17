111b8421236801e06ab7010e11ef9d5e43a29f7a
dmosc <osdavrm@gmail.com>
2022-09-17 18:40:42 -0500

    Add file component to [...path].tsx query parameter.
    This adds a final component: query/to/[file.ext]. This way, users can share the entire link and visitors get the file contents downloaded on first visit.
    
    * Added blocking logic to back button to in the resources tab so users won't be able to go back past the `repo` query parameter.

--- pages/explorer/[chat]/[owner]/resources.tsx
+++ pages/explorer/[chat]/[owner]/resources.tsx
@@ -12,65 +12,71 @@ interface Props {
 const Resources: React.FC<Props> = ({ setCode }) => {
   const router = useRouter();
   const { owner, path } = router.query;
+  const [repo, ...currentPath] = path as string[];
   const [resources, setResources] = useState<
-    Endpoints["GET /repos/{owner}/{repo}/contents/{path}"]["response"]["data"][]
+    Endpoints["GET /repos/{owner}/{repo}/contents/{path}"]["response"]["data"]
   >([]);
   useEffect(() => {
-    const [repo, ...rest] = path as string[];
     github
-      .query(`/repos/${owner}/${repo}/contents/${rest.join("/")}`)
+      .query(`/repos/${owner}/${repo}/contents/${currentPath.join("/")}`)
       .then(({ data }) => {
-        setResources(data);
+        const isFile = !Array.isArray(data);
+        if (isFile) {
+          github
+            .query(`/repos/${owner}/${repo}/git/blobs/${data.sha}`)
+            .then(({ data }) => {
+              setCode(atob(data.content).split("\n"));
+            })
+            .catch(ErrorManager.log);
+        }
+        setResources(isFile ? [data] : data);
       })
       .catch(ErrorManager.log);
-  }, [owner, path]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [path]);
   return (
     <div className={styles.container}>
       <div className={styles.containerHeader}>
         <h2 className={styles.containerTitle}>File explorer</h2>
-        <span
+        <button
+          disabled={!currentPath.length}
           className="material-icons-outlined"
           onClick={() => {
-            const lastSlash = router.asPath.lastIndexOf("/");
-            if (lastSlash) {
-              router.push(router.asPath.slice(0, lastSlash));
-            }
+            /*
+              `currentPath` contains everything from [...path].tsx query param after
+              the `repo` definition from index 0. It being empty means that there's
+              no path component left to pop from the current path.
+            */
+            const newPath = router.asPath.split("/");
+            newPath.pop();
+            router.push(newPath.join("/"));
           }}
         >
           arrow_back
-        </span>
+        </button>
       </div>
-      {resources.map((resource) => {
-        /*
-          TypeScript compiler does static Union resolution using surrounding boolean logic.
-          Github's response type for this endpoint can be a resource object or an array of these
-          resource objects. We need the TypeScript compiler to discard the array interface option
-          with an !Array.isArray(resource).
-        */
-        if (!Array.isArray(resource)) {
+      {Array.isArray(resources) &&
+        resources.map((resource) => {
+          /*
+            TypeScript compiler does static union resolution using surrounding boolean logic.
+            Github's response type for this endpoint can be a resource object or an array of these
+            resource objects. We need the TypeScript compiler to discard the array interface option
+            with an !Array.isArray(resources).
+          */
+          const isFileOpen =
+            resource.name === currentPath[currentPath.length - 1];
           return (
             <div
               key={resource.sha}
-              className={styles.resource}
+              className={`${styles.resource} ${isFileOpen ? "no-action" : ""}`}
               onClick={() => {
-                if (resource.type === "file") {
-                  const [repo] = path as string[];
-                  github
-                    .query(`/repos/${owner}/${repo}/git/blobs/${resource.sha}`)
-                    .then(({ data }) => {
-                      setCode(atob(data.content).split("\n"));
-                    })
-                    .catch(ErrorManager.log);
-                } else {
-                  router.push(`${router.asPath}/${resource.name}`);
-                }
+                router.push(`${router.asPath}/${resource.name}`);
               }}
             >
               {resource.name}
             </div>
           );
-        }
-      })}
+        })}
     </div>
   );
 };
