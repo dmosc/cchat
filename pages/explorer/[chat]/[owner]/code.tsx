4fbe8a03b286d8c367013345f26ee17288bcd21c
dmosc <osdavrm@gmail.com>
2022-09-19 21:56:42 -0500

    Add line highlighting functionality with query param update.
    Attaching the highlighted lines to a query param (`lines`) allows syncing clients -- or even sharing the link -- to copy over the state of the highlighted lines seamlessly.

--- pages/explorer/[chat]/[owner]/code.tsx
+++ pages/explorer/[chat]/[owner]/code.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import socketClient from "services/socket-client";
 import styles from "./code.module.css";
 
@@ -8,8 +9,39 @@ interface Props {
 
 const Code: React.FC<Props> = ({ code }) => {
   const router = useRouter();
-  const { chat } = router.query;
+  const { chat, lines } = router.query;
   const CODE_SYNC_EVENT = `__code_sync__${chat}`;
+  const [selectedRange, setSelectedRange] = useState({ min: -1, max: -1 });
+
+  useEffect(() => {
+    if (selectedRange.min >= 0 && selectedRange.max >= 0) {
+      let lines = String(selectedRange.min);
+      if (selectedRange.min !== selectedRange.max) {
+        lines = lines.concat(`-${selectedRange.max}`);
+      }
+      router.push(
+        {
+          pathname: router.pathname,
+          query: {
+            ...router.query,
+            lines
+          }
+        },
+        undefined,
+        { shallow: true } // Avoid refetching the page.
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedRange]);
+
+  useEffect(() => {
+    if (!!lines) {
+      const [min, max] = (lines as string).split("-").map(Number);
+      setSelectedRange({ min, max: max ?? min });
+    } else {
+      setSelectedRange({ min: -1, max: -1 });
+    }
+  }, [lines]);
 
   return (
     <div className={styles.container}>
@@ -35,8 +67,25 @@ const Code: React.FC<Props> = ({ code }) => {
           }}
         >
           {code.map((line, i) => {
+            const maybeSelectStyle =
+              i >= selectedRange.min &&
+              i <= selectedRange.max &&
+              styles.lineContainerSelected;
             return (
-              <tr key={i} className={styles.lineContainer}>
+              <tr
+                key={i}
+                className={`${styles.lineContainer} ${maybeSelectStyle}`}
+                onClick={(e) => {
+                  if (e.shiftKey && selectedRange.min >= 0) {
+                    setSelectedRange({
+                      min: Math.min(i, selectedRange.min),
+                      max: Math.max(i, selectedRange.min)
+                    });
+                  } else {
+                    setSelectedRange({ min: i, max: i });
+                  }
+                }}
+              >
                 <td className={styles.lineNumber}>{i}</td>
                 <td className={styles.lineContent}>
                   <span>{line ?? "&nbsp;"}</span>
